b7068d32d60bb544197c06eba36b10d5af610124
wavehackmail@gmail.com <wavehackmail@gmail.com@56e8d8f8-6cf7-11de-b5d2-8dea7b56f9cf>
2011-10-12 15:56:43 +0000

    Changed root redirection based on login_required setting.

--- application/controllers/welcome.php
+++ application/controllers/welcome.php
@@ -19,9 +19,19 @@ class Welcome extends CI_Controller
 		// If login is required and we're not logged in, redirect to login page
 		$this->AuthenticationModel->require_login(false, false);
 
-		// We're logged in (since above line didn't redirect to login). Original
-		// Redmine calls /my internally here, but we can't do that because we
-		// can't call another controller's function, so we'll just redirect.
-		redirect('/my');
+		if ($this->AuthenticationModel->is_logged_in())
+		{
+			// If we're logged in, redirect to /my. Redmine calls /my internally
+			// here, but we can't do that since we can't call another
+			// controller's method.
+			redirect('/my');
+		}
+		else
+		{
+			// We're not logged in and we don't need to, because setting
+			// login_required equals 0, so redirect to project overview.
+			// Todo: add setting for default landing page
+			redirect('/projects');
+		}
 	}
 }
